3ca7b985335f48ec27fd08fe71738e137c37727b
Дмитрий <dv@intervolga.ru>
2022-04-25 20:33:58 +0300

    заменил json_encode на вызов $app->json

--- rest/index.php
+++ rest/index.php
@@ -6,13 +6,14 @@ $app = new Silex\Application();
 
 //для групп:
 $app->get('/group/list.json', function () {
+	$app = new Silex\Application();
 	$group = new Groups;
 	$list = $group->read();
-	return json_encode($list);
+	return $app->json($list);
 });
 
 $app->post('/group/add-item', function () {
-
+	$app = new Silex\Application();
 	if ($_POST['groupName'] && $_POST['speciality']) {
 		$nameGroup = $_POST['groupName'];
 		$speciality = $_POST['speciality'];
@@ -20,15 +21,16 @@ $app->post('/group/add-item', function () {
 		try {
 			$group->create(array("groupName" => $nameGroup, "speciality" => $speciality));
 			$lastid = $group->lastID();
-			return json_encode(array("create-group" => "yes", "create-id" => $lastid));
+			return $app->json(array("create-group" => "yes", "create-id" => $lastid));
 		} catch (PDOException $e) {
-			return json_encode(array("error" => $e->getMessage(), "create-group" => "no"));
+			return $app->json(array("error" => $e->getMessage(), "create-group" => "no"));
 		}
 	} else {
-		return json_encode(array("create-group" => "no"));
+		return $app->json(array("create-group" => "no"));
 	}
 });
 $app->post('/group/update-item', function () {
+	$app = new Silex\Application();
 	$group = new Groups;
 	$idGroup = intval($_POST["idGroup"]);
 	$groupName = $_POST["groupName"];
@@ -36,39 +38,41 @@ $app->post('/group/update-item', function () {
 	if ($group->exists($idGroup) && $groupName) {
 		try {
 			$group->update(array("speciality" => $speciality, "id" => $idGroup, "groupName" => $groupName));
-			return json_encode(array("update-group" => "yes", "id_update" => $idGroup));
+			return $app->json(array("update-group" => "yes", "id_update" => $idGroup));
 		} catch (PDOException $e) {
-			return json_encode(array("error" => $e->getMessage(), "update-group" => "no"));
+			return $app->json(array("error" => $e->getMessage(), "update-group" => "no"));
 		}
 	} else {
-		return json_encode(array("update-group" => "no"));
+		return $app->json(array("update-group" => "no"));
 	}
 });
 
 $app->post('/group/delete-item', function () {
+	$app = new Silex\Application();
 	$group = new Groups;
 	$id = intval($_POST["id"]);
 	if ($group->exists($id)) {
 		try {
 			$group->delete($id);
-			return json_encode(array("delete-group" => "yes", "id_delete" => $id));
+			return $app->json(array("delete-group" => "yes", "id_delete" => $id));
 		} catch (PDOException $e) {
-			return json_encode(array("error" => $e->getMessage(), "delete-group" => "no"));
+			return $app->json(array("error" => $e->getMessage(), "delete-group" => "no"));
 		}
 	} else {
-		return json_encode(array("delete-group" => "no"));
+		return $app->json(array("delete-group" => "no"));
 	}
 });
 
 //для студентов:
 
 $app->get('/student/list.json', function () {
-
+	$app = new Silex\Application();
 	$student = new Students;
 	$list = $student->read();
-	return json_encode($list);
+	return $app->json($list);
 });
 $app->post('/student/add-item', function () {
+	$app = new Silex\Application();
 	$name = $_POST["name"];
 	$surname = $_POST["surname"];
 	$patronymic = $_POST["patronymic"];
@@ -79,15 +83,16 @@ $app->post('/student/add-item', function () {
 		try {
 			$student->create(array('name' => $name, "idGroup" => $idGroup, "surname" => $surname, "patronymic" => $patronymic));
 			$lastid = $student->lastID();
-			return json_encode(array("create-student" => "yes", "create-id" => $lastid));
+			return $app->json(array("create-student" => "yes", "create-id" => $lastid));
 		} catch (PDOException $e) {
-			return json_encode(array("error" => $e->getMessage(), "create-student" => "no"));
+			return $app->json(array("error" => $e->getMessage(), "create-student" => "no"));
 		}
 	} else {
-		return json_encode(array("create-student" => "no"));
+		return $app->json(array("create-student" => "no"));
 	}
 });
 $app->post('/student/update-item', function () {
+	$app = new Silex\Application();
 	$id = intval($_POST["id"]);
 	$name = $_POST["name"];
 	$surname = $_POST["surname"];
@@ -97,28 +102,28 @@ $app->post('/student/update-item', function () {
 	if ($student->exists($id) && $name) {
 		try {
 			$student->update(array("id" => $id, "name" => $name, "surname" => $surname, "patronymic" => $patronymic, "idGroup" => $idGroup));
-			return json_encode(array("update-student" => "yes", "id_update" => $id));
+			return $app->json(array("update-student" => "yes", "id_update" => $id));
 		} catch (PDOException $e) {
-			return json_encode(array("error" => $e->getMessage(), "update-student" => "no"));
+			return $app->json(array("error" => $e->getMessage(), "update-student" => "no"));
 		}
 	} else {
-		return json_encode(array("update-student" => "no"));
+		return $app->json(array("update-student" => "no"));
 	}
 });
 
 $app->post('/student/delete-item', function () {
-
+	$app = new Silex\Application();
 	$id = intval($_POST["id"]);
 	$student = new Students;
 	if ($student->exists($id)) {
 		try {
 			$student->delete($id);
-			return json_encode(array("delete-student" => "yes", "id_delete" => $id));
+			return $app->json(array("delete-student" => "yes", "id_delete" => $id));
 		} catch (PDOException $e) {
-			return json_encode(array("error" => $e->getMessage(), "delete-student" => "no"));
+			return $app->json(array("error" => $e->getMessage(), "delete-student" => "no"));
 		}
 	} else {
-		return json_encode(array("delete-student" => "no"));
+		return $app->json(array("delete-student" => "no"));
 	}
 });
 
